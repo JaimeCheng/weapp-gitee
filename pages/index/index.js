f4feea699c012056d8a189bfc27c3047a2d27cbb
JaimeCheng <403693553@qq.com>
2020-01-03 17:53:48 +0800

    index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,53 +2,49 @@
 //获取应用实例
 const app = getApp()
 
+const USER = require('../../api/user.js')
+const Base64 = require('../../utils/base64.js').Base64
+// const Towxml = require('../../towxml/main'); 
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    readme: '',
+    currThis: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
+    USER.demo('/JaimeCheng/e-signature').then(res => {
+      console.log(res) 
+      // let data = this.towxml.toJson(
+      //   Base64.decode(res.content),
+      //   'markdown' 
+      // );
+      // data = this.towxml.initData(data, {
+      //   base: '',    // 需要解析的内容中相对路径的资源`base`地址
+      //   app: this                     // 传入小程序页面的`this`对象，以用于音频播放器初始化
+      // });
+
+      //设置文档显示主题，默认'light'
+      // data.theme = 'dark';
+
+      // 设置数据
+      // this.setData({
+      //   readme1: data
+      // });  
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+        currThis: this,
+        readme: Base64.decode(res.content)
+      }) 
+      // this['event_bind_tap'] = (event) => {
+      //   console.log(event.target.dataset._el);     // 打印出元素信息
+      // };
+    }).catch(err => {})
   },
-  getUserInfo: function(e) {
+  // towxml: new Towxml(),
+  wxmlTagATap(e) {
+    console.log(e);
+  },
+  click (e) {
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
