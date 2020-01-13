b73508b0be80763af6b71e9a5f022ed74f4c7ad2
JaimeCheng <403693553@qq.com>
2020-01-13 15:17:32 +0800

    refresh token

--- miniprogram/utils/request.js
+++ miniprogram/utils/request.js
@@ -1,5 +1,5 @@
 import store from '../store/store.js'
-
+const redirectOld = require('./util.js').redirectOld
 const baseURL = 'https://gitee.com/api'
 
 const urlRegex = /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/|www\.)(([A-Za-z0-9-~]+)\.)+([A-Za-z0-9-~+(:)\/._])+$/
@@ -31,18 +31,56 @@ const request = (options) => {
         } else {
           console.log(res.data)
           reject(res.data)
-          // wx.showToast({
-          //   title: res.data.error_description || res.data.message,
-          //   icon: 'none',
-          //   duration: 2000
-          // })
           if (res.statusCode === 401) {
-            wx.removeStorageSync('token')
-            store.data.token = null
-            store.update()
+            if (store.data.token.refresh_token) {
+              const postdata = {
+                grant_type: 'refresh_token',
+                refresh_token: store.data.token.refresh_token
+              }
+              wx.request({
+                url: 'https://gitee.com/oauth/token',
+                method: 'POST',
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                data: postdata,
+                success(res) {
+                  store.data.token = res.data
+                  wx.setStorageSync('token', res.data)
+                  store.update()
+                  redirectOld()
+                },
+                fail(error) {
+                  console.log(error)
+                  wx.showToast({
+                    title: error.errMsg,
+                    icon: 'none',
+                    duration: 2000
+                  })
+                  wx.removeStorageSync('token')
+                  store.data.token = null
+                  store.update()
+                  wx.showToast({
+                    title: '登录凭证无效',
+                    icon: 'none'
+                  })
+                  reject(error.errMsg)
+                }
+              })
+            } else {
+              wx.removeStorageSync('token')
+              store.data.token = null
+              store.update()
+              wx.showToast({
+                title: '登录凭证无效',
+                icon: 'none'
+              })
+            }
+          } else {
             wx.showToast({
-              title: '登录凭证无效',
-              icon: 'none'
+              title: res.data.error_description || res.data.message,
+              icon: 'none',
+              duration: 2000
             })
           }
         }
