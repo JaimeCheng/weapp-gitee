34aeeca9707acdb4eeb4f1764651b426914d77ff
JaimeCheng <403693553@qq.com>
2020-02-15 17:58:55 +0800

    update components

---
+++ miniprogram/components/useritem/index.js
@@ -0,0 +1,26 @@
+// components/useritem/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userData: {
+      type: Object,
+      value: null
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
