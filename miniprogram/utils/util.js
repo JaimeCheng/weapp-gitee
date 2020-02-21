b43562d273ac6e8f6b70a24a2dd6062e4bcf0e97
JaimeCheng <403693553@qq.com>
2020-02-21 16:14:45 +0800

    add util

--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -31,7 +31,46 @@ const redirectOld = () => {
     })
   }
 }
+const getDateDiff = (dateTimeStamp) => {
+  var result = ''
+  var minute = 1000 * 60
+  var hour = minute * 60
+  var day = hour * 24
+  var halfamonth = day * 15
+  var month = day * 30
+  var year = month * 12
+  var now = new Date().getTime()
+  var diffValue = now - dateTimeStamp
+  if(diffValue < 0){return;}
+  var yearC = diffValue/year
+  var monthC =diffValue/month
+  var weekC =diffValue/(7*day)
+  var dayC =diffValue/day
+  var hourC =diffValue/hour
+  var minC =diffValue/minute
+  if(yearC>=1){
+    result="" + parseInt(yearC) + " 年前"
+  } else if(monthC>=1){
+      result="" + parseInt(monthC) + " 个月前"
+  }
+  else if(weekC>=1){
+      result="" + parseInt(weekC) + " 周前"
+  }
+  else if(dayC>=1){
+      result=""+ parseInt(dayC) +" 天前"
+  }
+  else if(hourC>=1){
+      result=""+ parseInt(hourC) +" 小时前"
+  }
+  else if(minC>=1){
+      result=""+ parseInt(minC) +" 分钟前"
+  }else
+  result="刚刚"
+  return result
+}
+
 module.exports = {
   formatTime: formatTime,
-  redirectOld: redirectOld
+  redirectOld: redirectOld,
+  getDateDiff: getDateDiff
 }
