94478233217f978e734021989f592a44965b3f77
JaimeCheng <403693553@qq.com>
2020-02-28 14:28:11 +0800

    update about

--- miniprogram/staticPack/about/author.js
+++ miniprogram/staticPack/about/author.js
@@ -8,9 +8,24 @@ Page({
 
   },
 
+  feedback: function () {
+    wx.navigateToMiniProgram({
+      appId: 'wx8abaf00ee8c3202e',
+      extraData: {
+        id: '125954'
+      }
+    })
+  },
+
+  toRepo: function () {
+    wx.navigateTo({
+      url: '../../pages/repo/home?path=JaimeCheng/weapp-gitee'
+    })
+  },
+
   copyIt: function () {
     wx.setClipboardData({
-      data: 'https://github.com/JaimeCheng',
+      data: 'https://github.com/JaimeCheng/weapp-gitee',
       success (res) {
         wx.showToast({
           title: '链接已复制！'
